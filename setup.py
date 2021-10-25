72e3c837b3dbe975b58775388b969bc405d908ec
wolvecap <672088256@qq.com>
2021-10-25 14:22:53 +0800

    update version info in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name="TAA",
     py_modules=["text-autoaugment"],
-    version="1.0",
+    version="2.0",
     description="augment your dataset with text auto augmentation",
     author="shuhuairen",
     packages=find_packages(),
