a62b345a47b923d5c424264ab6fdff9b69c79bb1
wolvecap <672088256@qq.com>
2021-10-25 14:29:52 +0800

    version 3.0 fixing import problem

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name="TAA",
     py_modules=["text-autoaugment"],
-    version="2.0",
+    version="3.0",
     description="augment your dataset with text auto augmentation",
     author="shuhuairen",
     packages=find_packages(),
